253c530d7e3e8dd9c2bc2e3009c634e4b107abeb
johannes.nogueira <johannes.nogueira@eduq.app>
2020-04-16 12:45:12 -0300

    challenge solution

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeInicial = 0;
+    const outcomeInicial = 0;
+
+    const income = this.transactions.reduce((acumulador, valorAtual) => {
+      if (valorAtual.type === 'income') return acumulador + valorAtual.value;
+      return acumulador;
+    }, incomeInicial);
+
+    const outcome = this.transactions.reduce((acumulador, valorAtual) => {
+      if (valorAtual.type === 'outcome') return acumulador + valorAtual.value;
+      return acumulador;
+    }, outcomeInicial);
+
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
